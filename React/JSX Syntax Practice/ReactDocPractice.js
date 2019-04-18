dac34778ea4d553cb53a1c879b965368c53f7177
Marlon Braga <rdg.brg@gmail.com>
2019-04-17 23:15:42 -0400

    React Documentation - Introducing JSX

---
+++ React/JSX Syntax Practice/ReactDocPractice.js
@@ -0,0 +1,42 @@
+const user = {
+  firstName: 'Marlon',
+  lastName: 'Braga'
+};
+
+const attributes = {
+  headColor: "colorBlue"
+};
+
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+function getGreeting(user, attr) {
+  if (user) {
+    return (
+      <div>
+        <h1 className={attr.headColor}>Hello, {formatName(user)}!</h1>
+        <h2>It is good to see you.</h2>
+      </div>
+    )
+  }
+  return <h1>Hello, Stranger.</h1>
+}
+
+const element = (
+  getGreeting(user, attributes)
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
+
+// const name = "Marlon Braga";
+// const element = <h1>Hello, {name}</h1>;
+
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
