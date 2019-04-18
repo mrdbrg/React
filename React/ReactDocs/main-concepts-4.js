446e0ca117d3a52ca098901571b44df6ae46fed2
Marlon Braga <rdg.brg@gmail.com>
2019-04-18 15:04:53 -0400

    main-concepts-4

--- React/ReactDocs/main-concepts-4.js
+++ React/ReactDocs/main-concepts-4.js
@@ -6,7 +6,8 @@ POINT#1:
 */
 
 class Clock extends React.Component {
-  constructor(props, totalSum) {
+// Step 2) constructor of the Clock is called and this.state is initialized
+  constructor(props) {
     super(props);
     this.state = {
       date: new Date()
@@ -14,23 +15,27 @@ class Clock extends React.Component {
   }
 
   // LIFECYCLE METHODS ( mounting and unmounting )
+    // Step 4) componentDidMount() lifecycle method is called. A timer is set up to call the component's tick() method once a second.
   componentDidMount() {
     this.timerID = setInterval(
       () => this.tick(), 1000
     );
   }
 
+  // *If Clock component is removed from the DOM, componentWillUnmount() lifecyle method is called and the timer will stop.*
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
 
+  // Step 5) Every second the browser calls the tick() methods. Clock component schedules a UI update by calling setState() with an object containing the current time. Render() method is called again to know what should go on the screen.
   tick() {
     this.setState({
       date: new Date()
     });
   }
 
-
+  // Step 3) render() method is called. React now knows what should displayed on the screen. React updates the DOM to match Clock's render output.
+  // Step 6) this.state.date in render() method will change. Render output will have the updated time.
   render() {
     return (
       <div>
@@ -41,33 +46,17 @@ class Clock extends React.Component {
   }
 }
 
+// Step 1) <Clock /> is passed
 ReactDOM.render(
   <Clock />,
   document.getElementById('root')
 );
 
-// Let’s quickly recap what’s going on and the order in which the methods are called:
-//
-// 1. When <Clock /> is passed to ReactDOM.render(), React calls the constructor of the Clock component.
-// Since Clock needs to display the current time, it initializes this.state with an object including the current time. We will later update this state.
-//
-// 2. React then calls the Clock component’s render() method. This is how React learns what should be displayed
-// on the screen. React then updates the DOM to match the Clock’s render output.
-//
-// 3. When the Clock output is inserted in the DOM, React calls the componentDidMount() lifecycle method.
-// Inside it, the Clock component asks the browser to set up a timer to call the component’s tick() method once a second.
-//
-// 4. Every second the browser calls the tick() method. Inside it, the Clock component schedules a UI update
-// by calling setState() with an object containing the current time. Thanks to the setState() call, React knows the state has changed, and calls the render() method again to learn what should be on the screen. This time, this.state.date in the render() method will be different, and so the render output will include the updated time. React updates the DOM accordingly.
-//
-// 5. If the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle method
-// so the timer is stopped.
-
 // source: https://bit.ly/2GYUKLK
 
-// ============================================================================
-//                               Basic Example
-// ============================================================================
+// =============================================================
+//                     Basic Example
+// =============================================================
 
 // setInterval(tick, 1000);
 
