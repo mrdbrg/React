8e87e381986231ccf5c6b486bcacea0cc080a9d0
Marlon Braga <rdg.brg@gmail.com>
2019-04-20 12:08:51 -0400

    main-concepts-8

---
+++ React/ReactDocs/main-concepts-8.js
@@ -0,0 +1,32 @@
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick(this);
+    this.handleLogoutClick = this.handleLogoutClick(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+}
