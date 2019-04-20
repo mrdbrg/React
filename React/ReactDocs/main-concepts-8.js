243257ee57e90f57ad43f47af86a50bcbbb9eff3
Marlon Braga <rdg.brg@gmail.com>
2019-04-20 12:40:33 -0400

    main-concepts-8

--- React/ReactDocs/main-concepts-8.js
+++ React/ReactDocs/main-concepts-8.js
@@ -1,3 +1,56 @@
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.handleLoginClick = this.handleLoginClick.bind(this);
+    // this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick = (event) => {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick = (event) => {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+
+    return (
+       <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+       </div>
+    );
+  }
+}
+
+// FUNCTIONS
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
 function LoginButton(props) {
   return (
     <button onClick={props.onClick}>
@@ -14,19 +67,7 @@ function LogoutButton(props) {
   );
 }
 
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick(this);
-    this.handleLogoutClick = this.handleLogoutClick(this);
-    this.state = {isLoggedIn: false};
-  }
-
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
-}
+ReactDOM.render(
+  <LoginControl />,
+  document.getElementById('root')
+);
