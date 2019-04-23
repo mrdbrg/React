faf9176fed2bf38a071b7e4c4d98d6e9f13aff99
Marlon Braga <rdg.brg@gmail.com>
2019-04-22 21:50:25 -0400

    main-concepts-11

--- React/ReactDocs/main-concepts-11.js
+++ React/ReactDocs/main-concepts-11.js
@@ -2,6 +2,26 @@
 Keys
 Keys help React identify which items have changed, are added, or are removed. Keys should be given to the elements inside the array to give the elements a stable identity.
 */
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => <ListItem key={number.toString()} value={number} /> );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
+const numbers = [1,2,3,4,5];
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
+);
+
 
 /*
 function NumberList(props) {
