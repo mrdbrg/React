feb86e1b0e114382ef99dd72d5a2733cbb1d7586
Marlon Braga <rdg.brg@gmail.com>
2019-04-29 01:13:32 -0400

    main-concepts-22

---
+++ React/ReactDocs/main-concepts-22.js
@@ -0,0 +1,24 @@
+function FancyBorder(props) {
+
+}
+
+function Dialog(props) {
+
+}
+
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {login: ''};
+  }
+
+  render() {
+
+  }
+  
+}
+
+ReactDOM.render(
+  <SignUpDialog />,
+  document.getElementById('root')
+);
