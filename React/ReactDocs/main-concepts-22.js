7f6f496fb4f6a34e63c6e115a0c458a4f3bca09d
Marlon Braga <rdg.brg@gmail.com>
2019-04-29 01:14:04 -0400

    main-concepts-22

--- React/ReactDocs/main-concepts-22.js
+++ React/ReactDocs/main-concepts-22.js
@@ -1,9 +1,23 @@
 function FancyBorder(props) {
+  return (
+    <div className={'FancyBorder FancyBorder-' + props.color}>
 
+    </div>
+  );
 }
 
 function Dialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="">
 
+      </h1>
+      <p className="">
+
+      </p>
+
+    </FancyBorder>
+  );
 }
 
 class SignUpDialog extends React.Component {
@@ -13,9 +27,22 @@ class SignUpDialog extends React.Component {
   }
 
   render() {
+    return (
+      <Dialog title="Mars Exploration Program"
+              message="How should we refer to you?">
 
+        <input />
+
+        <button onClick={this.handleSignUp}>
+
+        </button>
+
+      </Dialog>
+    );
   }
-  
+
+
+
 }
 
 ReactDOM.render(
