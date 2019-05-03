9712a76ba2828efd368aea825ce77a2c24cd8e35
Marlon Braga <rdg.brg@gmail.com>
2019-05-02 22:44:10 -0400

    Programming Pattern directory

---
+++ React/BoilerPlateProjects/Programming Pattern/stateless-from-stateful/Child.js
@@ -0,0 +1,11 @@
+/*
+  Programming Patter:
+  Stateless Components Inherit From Stateful Components
+*/
+import React from 'react';
+
+export class Child extends React.Component {
+  render() {
+    return <h1>Hey, my name is {this.props.name}!</h1>;
+  }
+ }
