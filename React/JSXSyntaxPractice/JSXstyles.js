8b18d94e0a7e66bb9fdcd3a48ce7f2d8aa3ef8f0
Marlon Braga <rdg.brg@gmail.com>
2019-05-09 15:44:25 -0400

    JSXSyntaxPractice/JSXstyles

--- React/JSXSyntaxPractice/JSXstyles.js
+++ React/JSXSyntaxPractice/JSXstyles.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 const styles = {
   background: 'lightblue',
   color: 			'darkred',
+  marginTop:  100,
+  fontSize:   50
 }
 
 const styleMe = <h1 style={styles}>Please style me! I am so bland!</h1>;
