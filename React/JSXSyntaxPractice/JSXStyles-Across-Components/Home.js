63b687d84904be58efcdeba63612dd08903e4a5c
Marlon Braga <rdg.brg@gmail.com>
2019-05-09 16:05:08 -0400

    JSXSyntaxPractice/JSXStyles-Across-Components

--- React/JSXSyntaxPractice/JSXStyles-Across-Components/Home.js
+++ React/JSXSyntaxPractice/JSXStyles-Across-Components/Home.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import { AttentionGrabber } from './AttentionGrabber';
 import { styles } from './styles';
 
+const divStyle = {
+  background: style.background,
+  height: '100%'
+}
+
 export class Home extends React.Component {
   render() {
     return (
