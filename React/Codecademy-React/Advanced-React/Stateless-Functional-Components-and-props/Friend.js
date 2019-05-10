5e94a43c68b38dcb2ce578e44844feb3addb2e9c
Marlon Braga <rdg.brg@gmail.com>
2019-05-10 19:15:14 -0400

    Stateless-Functional-Components-and-props

--- React/Codecademy-React/Advanced-React/Stateless-Functional-Components-and-props/Friend.js
+++ React/Codecademy-React/Advanced-React/Stateless-Functional-Components-and-props/Friend.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
+  A component class written as a function is called a stateless functional component.
+  Stateless functional components have some advantages over typical component classes. 
+*/
+
 export class Friend extends React.Component {
 	render() {
 		return <img src='https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-octopus.jpg' />;
