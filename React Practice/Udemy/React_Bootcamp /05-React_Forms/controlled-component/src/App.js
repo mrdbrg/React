a36903628f0e5247faa322a892b2e45529ee3536
Marlon Braga <rdg.brg@gmail.com>
2019-11-23 00:26:58 -0500

    Forms MultipleInputsLongWay

--- React Practice/Udemy/React_Bootcamp /05-React_Forms/controlled-component/src/App.js
+++ React Practice/Udemy/React_Bootcamp /05-React_Forms/controlled-component/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './App.css';
 import Form from './Form';
+import MultipleInputsLongWay from './MultipleInputsLongWay';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Form />
+        <MultipleInputsLongWay />
       </div>
     )
   }
