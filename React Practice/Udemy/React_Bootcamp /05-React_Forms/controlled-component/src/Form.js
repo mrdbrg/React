143bfdb12e7f568b2194a64a7c5694f79fcbda95
Marlon Braga <rdg.brg@gmail.com>
2019-11-22 23:11:27 -0500

    Form Demo

---
+++ React Practice/Udemy/React_Bootcamp /05-React_Forms/controlled-component/src/Form.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this);  
+    this.handleSubmit = this.handleSubmit.bind(this);  
+  }
+
+  handleChange(event) {
+    this.setState({ username: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+    alert("Your name is: " + this.state.username)
+    this.setState({ username: "" })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <p>What is your name?</p>
+          <input type="text" value={this.state.username} onChange={this.handleChange}/>
+          <button> Add</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Form
