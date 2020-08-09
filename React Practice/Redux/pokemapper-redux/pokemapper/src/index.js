e7748e12cc5e34ad8160a42678d352a19fe856df
github username <rdg.brg@gmail.com>
2020-08-09 14:40:38 -0400

    Create Provider aat the top level of component hierarchy with store as a prop

--- React Practice/Redux/pokemapper-redux/pokemapper/src/index.js
+++ React Practice/Redux/pokemapper-redux/pokemapper/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
+import store from './store';
 
 ReactDOM.render(
-    <App />,
+  <Provide store={store}>
+    <App />
+  </Provide>,  
   document.getElementById('root')
 );
 
