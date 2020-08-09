db12928850597f99133ede221b72c4fa064730a8
github username <rdg.brg@gmail.com>
2020-08-09 17:35:27 -0400

    Set up backeend

--- React Practice/Redux/pokemapper-redux/pokemapper-server/db/seeds.rb
+++ React Practice/Redux/pokemapper-redux/pokemapper-server/db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Stat.destroy_all
+Pokemon.destroy_all
+
+pokemons = [
+  {  
+    "name": "quagsire", 
+    "back_sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/195.png", 
+    "front_sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/195.png", 
+    "stats": [{ "base_stat": 35, "name": "speed" }, { "base_stat": 65, "name": "special-defense" }, { "base_stat": 65, "name": "special-attack" }, { "base_stat": 85, "name": "defense" }, { "base_stat": 85, "name": "attack" }], 
+    "lat": 40.699641625343126, 
+    "lng": -73.98946603152181 
+  }, 
+  { 
+    "name": "aurorus", 
+    "back_sprite": nil, 
+    "front_sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/699.png", 
+    "stats": [{ "base_stat": 58, "name": "speed" }, { "base_stat": 92, "name": "special-defense" }, { "base_stat": 99, "name": "special-attack" }], 
+    "lat": 40.701235859047294, 
+    "lng": -73.98229625164312 
+  }, 
+  { 
+    "name": "kingler", 
+    "back_sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/99.png", 
+    "front_sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/99.png", 
+    "stats": [{ "base_stat": 75, "name": "speed" }, { "base_stat": 50, "name": "special-defense" }, { "base_stat": 50, "name": "special-attack" }], 
+    "lat": 40.69547691646669, 
+    "lng": -73.9829402438478 
+  }
+]
+
+pokemons.each do |poke| 
+  pokemon = Pokemon.create(
+    name: poke[:name],
+    front_sprite: poke[:front_sprite],
+    back_sprite: poke[:back_sprite],
+    lat: poke[:lat],
+    lng: poke[:lng]
+  )
+
+  poke[:stats].each do |poke_stats| 
+    Stat.create(
+      name: poke_stats[:name],
+      base_stat: poke_stats[:base_stat],
+      pokemon_id: pokemon.id
+    )
+  end
+end
+
+puts "========================="
+puts "         SEEDED"
+puts "========================="
